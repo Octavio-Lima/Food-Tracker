c771e7bc89391aa62b828ac3afdd9a11c8c3c103
Octavio <116118454+Octavio-Lima@users.noreply.github.com>
2024-08-31 12:39:26 -0300

    Calcular apenas elementos visiveis

--- src/core/footer/update-footer.ts
+++ src/core/footer/update-footer.ts
@@ -4,8 +4,13 @@ export const updateFooter = (table: HTMLElement, footer: HTMLElement) => {
     let fields = ["kcal", "carb", "fats", "prot", "fibe", "sodi"];
     let units = ["kcal", "g", "g", "g", "g", "mg"];
 
+    console.log("asd");
+
     fields.forEach((f, i) => {
-        let val = Array.from(table.querySelectorAll(`.${f}`)).reduce((v, c) => (v += parseInt(c.textContent!)), 0);
+        let val = Array.from(table.querySelectorAll(`.${f}`))
+            .filter((e) => !e.parentElement!.classList.contains("hide"))
+            .reduce((v, c) => (v += parseInt(c.textContent!)), 0);
+
         Forms.setText(footer, `.total-${f}`, `${val.toString()}${units[i]}`);
     });
 };
